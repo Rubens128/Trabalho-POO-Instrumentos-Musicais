812fc13c741d84187b14cd0eda8e4a1a420b7848
Rubens de Oliveira Cristiano <rubens.cristiano@a.ucb.br>
2025-10-28 18:46:29 -0300

    feat(InterfaceGrafica): Adição da interface grafica da view

---
+++ TrabalhoPOOInstrumentosMusicais/src/main/java/view/TrabalhoPOOInstrumentosMusicais.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package view;
+
+/**
+ *
+ * @author ruben
+ */
+public class TrabalhoPOOInstrumentosMusicais {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            
+            /*TelaInstrumentos telaInstrumentos = new TelaInstrumentos();;;
+            telaInstrumentos.setVisible(true);
+            
+            telaInstrumentos.addInstrumentoCard("teste", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", true);
+            telaInstrumentos.addInstrumentoCard("teste", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", true);
+            telaInstrumentos.addInstrumentoCard("teste", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", true);
+            telaInstrumentos.addInstrumentoCard("teste", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", true);
+            telaInstrumentos.addInstrumentoCard("teste", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", true);
+            telaInstrumentos.addInstrumentoCard("teste", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", true);
+            telaInstrumentos.addInstrumentoCard("teste", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", true);
+            telaInstrumentos.addInstrumentoCard("teste", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", true);
+            telaInstrumentos.addInstrumentoCard("teste2", "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", true);*/
+            
+            /*InterfaceInputPadrao interfaceTeste = new InterfaceInputPadrao("Afinação");;;
+            interfaceTeste.setVisible(true);*/
+            
+            InterfaceInputAudio interfaceTeste = new InterfaceInputAudio();
+            interfaceTeste.setVisible(true);
+        });
+    }
+}
